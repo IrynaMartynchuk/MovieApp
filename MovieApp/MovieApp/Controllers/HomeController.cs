a652048f248b8ae3c2676fa8de235fbd870a2ca9
ira <ira@DESKTOP-30H1HLP>
2018-10-08 17:41:24 -0700

    working version of cart

--- MovieApp/MovieApp/Controllers/HomeController.cs
+++ MovieApp/MovieApp/Controllers/HomeController.cs
@@ -44,80 +44,70 @@ namespace MovieApp.Controllers
 
         public ActionResult ListMovies()
         {
+
             var db = new DBContext();
-                var order = (from orde in db.Orders where orde.Confirmed == false select orde.Id).ToList();
-                if (order.Count > 3)
-                {
-                    Order orders = new Order();
-                foreach(var lines in order)
-                {
-                    var delete = db.Orders.Find(lines);
-                    db.Orders.Remove(delete);
-                    db.SaveChanges();
-                }
-                        
-                }
-            else
+            Order falseOrder = db.Orders.SingleOrDefault(x => x.SessionId != this.Session.SessionID && x.Confirmed == false);
+            if(falseOrder != null)
             {
+                db.Orders.Remove(falseOrder);
+                db.SaveChanges();
+                
+            }
                 if (Session["Cart"] == null || (bool)Session["Cart"] == false)
                 {
-                    //var db = new DBContext();
                     string sessionId = this.Session.SessionID;
-                    db.SaveChanges();
+                var orderExist = db.Orders.Where(x => x.Confirmed == false).SingleOrDefault();
+                if (orderExist == null)
+                {
                     Order newOrder = new Order
                     {
                         Date = DateTime.Now,
                         Confirmed = false,
                         SessionId = sessionId
                     };
-
                     db.Orders.Add(newOrder);
                     db.SaveChanges();
                     Session["Cart"] = true;
                 }
-            }
-            
-            
-            
-                
+                }
             var Db = new DBMovie();
             List<Movie> allMovies = Db.retrieveAll();
             return View(allMovies);
         }
 
         public int addToCart(int MovieId)
+        {
+            var totalAmount = 0;
+            var db = new DBContext();
+            if (Session["Cart"] != null)
             {
-                var totalAmount = 0;
-                var db = new DBContext();
-                if (Session["Cart"] != null)
+
+                var newOrderline = new Orderline
+                {
+                    Antall = 1,
+                    MovieId = MovieId,
+                    OrderId = (from order in db.Orders where this.Session.SessionID == order.SessionId && order.Confirmed == false select order.Id).SingleOrDefault()
+                };
+                if (checkOrderline(newOrderline))
                 {
 
-                    var newOrderline = new Orderline
-                    {
-                        Antall = 1,
-                        MovieId = MovieId,
-                        OrderId = (from order in db.Orders where order.SessionId == this.Session.SessionID select order.Id).First()
-                    };
-                    if (checkOrderline(newOrderline))
-                    {
-
-                        var orderlines = new List<Orderline>();
-                        orderlines.Add(newOrderline);
-                        totalAmount = totalAmount + 1;
-                    }
-                    else
-                    {
-                        ViewBag.Error = "Item is already added to your cart!";
-                    }
-
+                    var orderlines = new List<Orderline>();
+                    orderlines.Add(newOrderline);
+                    totalAmount = totalAmount + 1;
                 }
-                return totalAmount;
+                else
+                {
+                    ViewBag.Error = "Item is already added to your cart!";
+                }
+
+            }
+            return totalAmount;
         }
 
         public bool checkOrderline(Orderline newOrderline)
         {
             var db = new DBContext();
-            var existingMovieId = db.Orderlines.Where(a => a.MovieId == newOrderline.MovieId).SingleOrDefault();
+            var existingMovieId = db.Orderlines.Where(a => a.MovieId == newOrderline.MovieId && a.OrderId == newOrderline.OrderId).SingleOrDefault();
             if (existingMovieId == null)
             {
                 try
@@ -141,8 +131,8 @@ namespace MovieApp.Controllers
         {
             var db = new DBContext();
 
-   
-            
+
+
             var orderId = (from order in db.Orders where order.SessionId == this.Session.SessionID && order.Confirmed == false select order.Id).SingleOrDefault();
             var orderlines = (from list in db.Orderlines where list.OrderId == orderId select list.MovieId).ToList();
             var movies = (from movie in db.Movies.AsEnumerable()
@@ -169,10 +159,11 @@ namespace MovieApp.Controllers
                 db.SaveChanges();
                 return true;
             }
-            catch(Exception error)
+            catch (Exception error)
             {
                 return false;
             }
+        
         }
 
         public ActionResult Login()
@@ -192,14 +183,15 @@ namespace MovieApp.Controllers
         {
             var db = new DBContext();
             var loggedIn = db.Customers.SingleOrDefault(c => c.Email == Customer.Email && c.Password == Customer.Password);
-            
+
             if (loggedIn != null)
             {
                 ViewBag.message = "You are logged in";
                 ViewBag.triedOnce = true;
 
                 Session["customer"] = Customer.Id;
-                
+                Order order = db.Orders.SingleOrDefault(x => x.Confirmed == false);
+                order.Customer = Customer;
                 return RedirectToAction("Index", "Home", new { customer = Customer.Id });
             }
             else
@@ -230,17 +222,24 @@ namespace MovieApp.Controllers
                 var message = "In order to procceed you need to log in!";
                 return message;
             }
-            var ok = "You will receive confirmation email with receipt!";
-            return ok;
-        }
+            else
+            {
+                var ok = "You will receive confirmation email with receipt!";
+                changeConfirmationStatus();
+                return ok;
+            }
+            
 
-        public void changeConfirmationStatus(int id)
+
+        } 
+        
+
+        public void changeConfirmationStatus()
         {
             if (Session["customer"] != null)
             {
                 var db = new DBContext();
-                var orderline = (from lines in db.Orderlines where lines.MovieId == id select lines.OrderId).First();
-                Order order = db.Orders.Single(x => x.Id == orderline);
+                Order order = db.Orders.Single(x => x.Confirmed == false);
                 order.Confirmed = true;
                 db.SaveChanges();
             }
