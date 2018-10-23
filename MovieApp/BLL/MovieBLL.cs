de6631ec42e85e1f97ee3f7bf2c0be268e174dbd
ira <ira@DESKTOP-30H1HLP>
2018-10-23 05:01:42 -0700

    admin movie

---
+++ MovieApp/BLL/MovieBLL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApp.DAL;
+using MovieApp.Model;
+
+namespace MovieApp.BLL
+{
+    public class MovieBLL
+    {
+        public List<Movie> retrieveAll()
+        {
+            var MovieDAL = new MovieDAL();
+            List<Movie> allMovies = MovieDAL.retrieveAll();
+            return allMovies;
+        }
+
+        public bool editMovie(int id, Movie inMovie)
+        {
+            var MovieDAL = new MovieDAL();
+            return MovieDAL.editMovie(id, inMovie);
+        }
+
+        public Movie viewDetails(int id)
+        {
+            var MovieDAL = new MovieDAL();
+            return MovieDAL.viewDetails(id);
+        }
+
+        public bool deleteMovie(int id)
+        {
+            var MovieDAL = new MovieDAL();
+            return MovieDAL.deleteMovie(id);
+        }
+    }
+}
--- MovieApp/DAL/MovieDAL.cs
+++ MovieApp/DAL/MovieDAL.cs
@@ -26,5 +26,74 @@ namespace MovieApp.DAL
             }
 
         }
+
+        public bool editMovie(int id, Movie movie)
+        {
+            var db = new DBContext();
+            var result = db.Movies.Find(id);
+
+            if (result != null)
+            {
+                result.ImageAddress = movie.ImageAddress;
+                result.Title = movie.Title;
+                result.Description = movie.Description;
+                result.Price = movie.Price;
+                result.Genre = movie.Genre;
+                try
+                {
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public Movie viewDetails(int id)
+        {
+            using (var db = new DBContext())
+            {
+                var movie = db.Movies.Find(id);
+
+                if (movie == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    var details = new Movie()
+                    {
+                        Id = movie.Id,
+                        ImageAddress = movie.ImageAddress,
+                        Title = movie.Title,
+                        Description = movie.Description,
+                        Genre = movie.Genre,
+                        Price = movie.Price
+                    };
+                    return details;
+                }
+            }
+        }
+
+        public bool deleteMovie(int id)
+        {
+            using (var db = new DBContext())
+            {
+                try
+                {
+                    Movie movie = db.Movies.Find(id);
+                    db.Movies.Remove(movie);
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
