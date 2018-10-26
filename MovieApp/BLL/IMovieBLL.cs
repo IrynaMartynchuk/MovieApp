cc84893b9f6d02df3b74a4b8886c4596360f8212
marimikh <mburunduk@gmail.com>
2018-10-26 13:41:41 +0200

    MovieStubs

---
+++ MovieApp/BLL/IMovieBLL.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MovieApp.Model;
+
+namespace MovieApp.BLL
+{
+    public interface IMovieBLL
+    {
+        bool addMovie(Movie newMovie);
+        bool editMovie(int id, Movie inMovie);
+        List<Movie> retrieveAll();
+        Movie viewDetails(int id);
+    }
+}
--- MovieApp/BLL/MovieBLL.cs
+++ MovieApp/BLL/MovieBLL.cs
@@ -9,31 +9,44 @@ using MovieApp.Model;
 
 namespace MovieApp.BLL
 {
-    public class MovieBLL
+    public class MovieBLL : BLL.IMovieBLL
     {
+        private IMovieRepository _repository;
+
+        public MovieBLL()
+        {
+            _repository = new MovieDAL();
+        }
+
+        public MovieBLL(IMovieRepository stub)
+        {
+            _repository = stub;
+        }
+
+
         public List<Movie> retrieveAll()
         {
-            var MovieDAL = new MovieDAL();
-            List<Movie> allMovies = MovieDAL.retrieveAll();
+           // var MovieDAL = new MovieDAL();
+            List<Movie> allMovies = _repository.retrieveAll();
             return allMovies;
         }
 
         public Movie viewDetails(int id)
         {
-            var movieDAL = new MovieDAL();
-            return movieDAL.viewDetails(id);
+           // var movieDAL = new MovieDAL();
+            return _repository.viewDetails(id);
         }
 
         public bool editMovie(int id, Movie inMovie)
         {
-            var MovieDAL = new MovieDAL();
-            return MovieDAL.editMovie(id, inMovie);
+            //var MovieDAL = new MovieDAL();
+            return _repository.editMovie(id, inMovie);
         }
 
         public bool addMovie(Movie newMovie)
         {
-            var movieDAL = new MovieDAL();
-            bool insertOK = movieDAL.addMovie(newMovie);
+            //var movieDAL = new MovieDAL();
+            bool insertOK = _repository.addMovie(newMovie);
             return insertOK;
         }
 
