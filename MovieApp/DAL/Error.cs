f48d315ea0b2f80a9fea8272fe1e36e5332f7f32
ira <ira@DESKTOP-30H1HLP>
2018-10-28 12:34:35 -0700

    commit

--- MovieApp/DAL/Error.cs
+++ MovieApp/DAL/Error.cs
@@ -15,7 +15,7 @@ namespace MovieApp.DAL
                 StreamWriter streamWriter = new StreamWriter(Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, @"..\\DAL\error_log.txt"), true);
                 streamWriter.WriteLine(DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + 
                     " " + DateTime.Now.ToShortDateString().ToString() + " " + DateTime.Now.ToLongTimeString().ToString() + " ==>"+ ": "
-                    + method + " ==> " + "==>" + e.Message + (e.InnerException == null ? "" : ("==>" + e.InnerException.Message)));
+                    + method + " ==> " + "==>" + e.Message + (e.InnerException == null ? "" : ("==>" + e.InnerException.Message)) + "<br/>");
                 streamWriter.Close();
             }
             catch (Exception)
